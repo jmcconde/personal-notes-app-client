6e61bed25ae97804adf4d6d81aa89967e6a1fc01
João Conde <joao997conde@gmail.com>
2023-02-16 00:21:51 +0000

    Remove console logs

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -2,7 +2,6 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from 'firebase/auth'
 
 // Your web app's Firebase configuration
-console.log(process.env.REACT_APP_FIREBASE_CONFIG);
 const firebaseConfig = JSON.parse(process.env.REACT_APP_FIREBASE_CONFIG);
 
 // Initialize Firebase
